f8bdcd4083c8942e2b08e069fc60fbd2b9a8d5dd
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-05-07 10:40:21 +0530

    v=int(input()) a=0 c=0 for i in range(1,v+1,1):     if v%i==0:         c+=1 if c!=2:     print('Not Mega Prime') else:     while v:         r=v%10         c=0         for i in range(1,r+1,1):                if r%i==0:                 c+=1         if c!=2:             a+=1         v=v//10     if a==0:         print('Mega Prime')     else:         print('Not Mega Prime')

--- mega_prime.py
+++ mega_prime.py
@@ -1,24 +1,22 @@
-n=int(input())
-v=0
-p=0
-m=0
-for i in range(1,n+1,1):
-      if(n%i==0):
-          v+=1
-if(v!=2):
+v=int(input())
+a=0
+c=0
+for i in range(1,v+1,1):
+    if v%i==0:
+        c+=1    
+if c!=2:
     print('Not Mega Prime')
 else:
-    while(n>0):
-        k=0
-        r=n%10
-        for j in range(1,r+1,1):
-            if(r%j==0):
-                k+=1
-        if(k==2):
-            m+=1
-        n=n//10
-        p+=1
-    if(p==m):
+    while v:
+        r=v%10
+        c=0
+        for i in range(1,r+1,1):
+               if r%i==0:
+                c+=1
+        if c!=2:
+            a+=1
+        v=v//10
+    if a==0:
         print('Mega Prime')
     else:
         print('Not Mega Prime')
