e0a068eeb15e7cae5b1171eeefa0836fa98e808e
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-07-17 01:01:33 +0530

    _=int(input()) a=list(map(int,input().split())) b=[] c=0 for i in a:     if i not in b:         b.append(i) for i in b:     if i==a.count(i):         print(i,end=' ')         c=1 if c==0:     print('-1')

---
+++ Elements_same_as_count.py
@@ -0,0 +1,13 @@
+_=int(input())
+a=list(map(int,input().split()))
+b=[]
+c=0
+for i in a:
+    if i not in b:
+        b.append(i)
+for i in b:
+    if i==a.count(i):
+        print(i,end=' ')
+        c=1
+if c==0:
+    print('-1')
