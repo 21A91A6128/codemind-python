7dc4729c113c025c8bc47354c8574ddcf5363251
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-08-20 00:06:04 +0530

    b=[] vow=['a','e','i','o','u'] for i in (input().lower()).split():     c=0     for j in list(i):         if j in vow:             c+=1     b.append(c) print(b.count(min(b)))

--- minimum_vowel_count_words.py
+++ minimum_vowel_count_words.py
@@ -1,19 +1,9 @@
-s=input().split(' ')
-vow=['a','e','i','o','u','A','E','I','O','U']
 b=[]
-d=0
-for i in s:
-    a=0
-    for j in i:
+vow=['a','e','i','o','u']
+for i in (input().lower()).split():
+    c=0
+    for j in list(i):
         if j in vow:
-            a+=1
-    b.append(a)
-c=min(b)
-for i in s:
-    a=0
-    for j in i:
-        if j in vow:
-            a+=1
-    if c==a:
-        d+=1
-print(d)
+            c+=1
+    b.append(c)
+print(b.count(min(b)))
