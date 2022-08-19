0be873962c8c1fed0b4c90119e77dff56f58f6ed
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-08-20 01:10:42 +0530

    vow=['a','e','i','o','u','A','E','I','O','U'] s=input() a=[] for i in s:     if i in vow:         a.append(i) a.reverse() b=0 for i in s:     if i in a:         i=a[b]         b+=1     print(i,end='')

---
+++ Reverse_Vowels_of_a_String.py
@@ -0,0 +1,13 @@
+vow=['a','e','i','o','u','A','E','I','O','U']
+s=input()
+a=[]
+for i in s:
+    if i in vow:
+        a.append(i)
+a.reverse()
+b=0
+for i in s:
+    if i in a:
+        i=a[b]
+        b+=1
+    print(i,end='')
