c39f7db493fd68f4f2063271a6770b7383ce2706
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-10-05 14:31:40 +0530

    Update Maximum_Number_of_Balloons.py

--- Maximum_Number_of_Balloons.py
+++ Maximum_Number_of_Balloons.py
@@ -1,8 +1,3 @@
-s=input()
-txt=['a','b','l','o','n']
-vk=''
-for i in s:
-    if i in txt:
-        vk+=i
+vk=input()
 ans=[vk.count('a'),vk.count('b'),vk.count('l')//2,vk.count('o')//2,vk.count('n')]
 print(min(ans))
