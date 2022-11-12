c9ce61cf55926c47cccbc6b3afea5aa5c1694a22
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-11-12 15:01:15 +0530

    Update Reverse_Palindrome.py

--- Reverse_Palindrome.py
+++ Reverse_Palindrome.py
@@ -1,25 +1,7 @@
-def rev(n):
-    r=s=0
-    while(n):
-        r=n%10
-        s=s*10+r
-        n//=10
-    return s
-def pn(n):
-    r=s=0
-    t=n
-    while(n):
-        r=n%10
-        s=s*10+r
-        n//=10
-    if t==s:
-        return 1
-    else:
-        return 0
 def rp(n):
-    r=rev(n)
+    r=int(str(n)[::-1])
     n+=r
-    if pn(n):
+    if str(n)==str(n)[::-1]:
         print(n)
         return 0
     else:
